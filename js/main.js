c9091ae8a54f4c3976c61a3b3c25e99dcff5dadb
tomiwa abiola <tomiwabiola02@gmail.com>
2022-03-16 06:23:15 +0800

    First save of Core Apparelng

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const mainMenu = document.querySelector('.mainMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
+
+openMenu.addEventListener('click', show)
+closeMenu.addEventListener('click', closee)
+
+function show(){
+    mainMenu.style.display = 'flex'; 
+    mainMenu.style.top = '0';
+}
+function closee(){
+    mainMenu.style.top = '-100%';
+}
+
+
