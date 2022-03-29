995c4ada5523bfe836f385a9447f5b6a2beda513
tomiwa abiola <tomiwabiola02@gmail.com>
2022-03-29 08:37:28 +0800

    adjustment of input to core apparel page

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 const mainMenu = document.querySelector('.mainMenu');
 const closeMenu = document.querySelector('.closeMenu');
 const openMenu = document.querySelector('.openMenu');
@@ -14,3 +15,11 @@ function closee(){
 }
 
 
+// thridPageMovingImage //
+let image = document.getElementById('thirdPageImage');
+let images =['/img/11.jpg','/img/2.jpg','/img/3.jpg','/img/4.jpg'];
+setInterval(function(){
+    let random = Math.floor(Math.random() * 4);
+    image.src = images[random];
+
+}, 2500);
